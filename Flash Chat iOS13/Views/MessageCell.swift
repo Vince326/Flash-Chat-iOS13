44a4d90a6803d1d58d551a155bd15718b6ea3e90
Vincent Hunter <jhearst29@gmail.com>
2021-02-28 13:36:10 -0500

    I have added the IQKeyboardManager dependency to allow the user to interact with the textbox and not be blocked by the keyboard. This dependency also has the ability to dismiss keyboard when the user touches the area outside of it.

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,29 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Vincent Hunter on 2/24/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    @IBOutlet var messageBubble: UIView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var rightImageView: UIImageView!
+    @IBOutlet var leftImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
